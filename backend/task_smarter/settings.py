31d74fb13376983392661d96191d6a68d2b480ae
Aiswarya Pokharel <pokharelaishwarya22@gmail.com>
2026-07-24 16:41:58 +0545

    fixed database url

--- backend/task_smarter/settings.py
+++ backend/task_smarter/settings.py
@@ -107,7 +107,7 @@ WSGI_APPLICATION = 'task_smarter.wsgi.application'
 # }
 
 DATABASES = {
-     'default': dj_database_url.config(env='DB_URL', conn_max_age=600)
+     'default': dj_database_url.config(env='DATABASE_URL', conn_max_age=600)
 }
 
 # Password validation
